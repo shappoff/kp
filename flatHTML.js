dd48f070296f3eb08d9aac66e02b6a547327b939
Sergey Shappo <shappoff@gmail.com>
2023-03-23 08:42:02 +0300

    add flat html file

---
+++ flatHTML.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const file_path = `./krupskiy/index.html`;
+
+const removeEmptyLines = str => str.split(/\r?\n/).filter(line => line.trim() !== '').map(e => e.trim()).join('\n');
+
+try {
+    const data = fs.readFileSync(file_path, 'utf8');
+
+    const ns = data.replace(/</g, '\n<').replace(/>/g, '>\n');
+
+    const res = removeEmptyLines(ns);
+    fs.writeFileSync(file_path, res, {encoding: 'utf8', flag: 'w'});
+} catch (err) {
+    console.error(err);
+}
