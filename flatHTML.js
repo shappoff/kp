da82e18a8b29cdb67eddc09f96c84d0f657e55ff
Sergey Shappo <shappoff@gmail.com>
2023-03-23 17:10:37 +0300

    formatting

--- flatHTML.js
+++ flatHTML.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const file_path = `./krupskiy/index.html`;
+const file_path = `./tolochinskiy/tolo4in.htm`;
 
 const removeEmptyLines = str => str.split(/\r?\n/).filter(line => line.trim() !== '').map(e => e.trim()).join('\n');
 
