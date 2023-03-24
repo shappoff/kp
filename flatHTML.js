00780b2c082f9e45aa94746c5576da160040354b
Sergey Shappo <shappoff@gmail.com>
2023-03-24 09:58:45 +0300

    remove tags ul and li

--- flatHTML.js
+++ flatHTML.js
@@ -4,13 +4,21 @@ const file_path = `./tolochinskiy/index.html`;
 
 const removeEmptyLines = str => str.split(/\r?\n/).filter(line => line.trim() !== '').map(e => e.trim()).join('\n');
 
+const removeTags = (str, tag) => {
+    const regexUL = /<\/?ul[^>]*>/g;
+    const regexLI = /<\/?li[^>]*>/g;
+
+    return  str.replace(regexUL, '').replace(regexLI, '');
+}
+
 try {
     const data = fs.readFileSync(file_path, 'utf8');
 
     const ns = data.replace(/</g, '\n<').replace(/>/g, '>\n');
 
-    const res = removeEmptyLines(ns);
-    fs.writeFileSync(file_path, res, {encoding: 'utf8', flag: 'w'});
+    const withoutEmtyLines = removeEmptyLines(ns);
+    const withoutLIandULTags = removeTags(withoutEmtyLines);
+    fs.writeFileSync(file_path, withoutLIandULTags, {encoding: 'utf8', flag: 'w'});
 } catch (err) {
     console.error(err);
 }
